e099d15f39ca904f0284c8c3ef8d4db6faa1011b
Arijit Mukherjee <arijitmukherjee1603@gmail.com>
2026-07-16 04:07:24 +0530

    Small fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Contact from "./sections/Contact";
 import Experience from "./sections/Experience";
 import Home from "./sections/Home";
 import Skills from "./sections/Skills";
-import Project from "./sections/project";
+import Project from "./sections/Project";
 import Footer from "./sections/Footer";
 import ParticlesBackground from "./components/ParticlesBackground";
 import CustomCursor from "./components/CustomCursor";
